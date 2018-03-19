07fdd4ad7b8e9e83ecf2a74ceb18579eb3ce129d
Habigaelle <lucie.laurent.bio@orange.fr>
2018-03-19 16:10:02 +0100

    Rajoute une exception.

--- src/com/llaurent/program.java
+++ src/com/llaurent/program.java
@@ -1,9 +1,16 @@
 package com.llaurent;
 
+import java.io.File;
+
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// C'est trop puissant !
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		}
 
 	}
 
